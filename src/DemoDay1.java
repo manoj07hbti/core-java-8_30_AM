c68d049c183d8bc5777faaeedabf806b00d29710
60manojkumar@gmail.com <Motivated@2019>
2022-08-18 08:49:49 +0530

    Sharing Project on Day 1

---
+++ src/DemoDay1.java
@@ -0,0 +1,9 @@
+public class DemoDay1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, This is my first java class...");
+
+    }
+
+}
---
+++ src/DemoDay2.java
@@ -0,0 +1,17 @@
+public class DemoDay2 {
+
+// SYNTAX : public static void main (String []args){
+//                //CODE
+//             }
+
+
+    public static void main(String[] args) {
+
+     System.out.println("Hello, Welcome to java ");
+     System.out.println("Hello, Welcome to java ");
+     System.out.println("I am new to Java ... ");
+     System.out.println("sjdjsjdsajdsadksakjdsakdad");
+
+    }
+
+}
