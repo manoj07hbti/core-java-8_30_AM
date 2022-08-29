5b64d03918b1a563d810580aff756c7c5d0a49bf
60manojkumar@gmail.com <Motivated@2019>
2022-08-29 08:54:36 +0530

    Loops

---
+++ src/loops/DemoLoop.java
@@ -0,0 +1,26 @@
+package loops;
+
+public class DemoLoop {
+    public static void main(String[] args) {
+
+
+        //TODO SYNTAX : for(initialization; condition; increment){
+        //                     //statement or code to be executed
+        //                 }
+
+        for (int i=1; i<=10; i++ ){
+
+            System.out.println("Hello ....:" +i);
+        }
+
+
+        //TODO SYNTAX : for(initialization; condition; decrement){
+        //                     //statement or code to be executed
+        //                 }
+
+        for (int i=10; i>0; i--){
+
+            System.out.println("Decrement for Loop :"+i);
+        }
+    }
+}
---
+++ src/loops/TableTwo.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class TableTwo {
+    public static void main(String[] args) {
+
+        int two=2;
+
+        for (int i=1; i<=10; i++){
+
+            int multiply= two*i;
+         System.out.println( two +" X "+i+ " = " +multiply );
+        }
+
+    }
+}
