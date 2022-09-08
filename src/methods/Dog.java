b07bad111ec5cee8f6f173519f3494142ead0967
60manojkumar@gmail.com <Motivated@2019>
2022-09-08 08:50:40 +0530

    Methods Day1

---
+++ src/methods/Dog.java
@@ -0,0 +1,39 @@
+package methods;
+
+public class Dog {
+
+    //data members
+    String breed;
+    int height;
+    int weight;
+
+
+    // methods or function or capabilities
+
+    // bark , eat , play
+ /* TODO
+        SYNTAX : access_specifier return_type  method_name (parameter) {CODE..}
+        ..
+        access_specifier -> public , private , protected , default   THEORY PART
+        return_type   -> output of function or method :  void : no return type
+        method_name -> can be anything: it should be meaningful
+        parameter ->  input parameter : optional*/
+
+//SYNTAX : access_specifier return_type  method_name (parameter) {CODE..}
+
+    public void barkMethod () {
+
+        System.out.println("This is bark method of Dog Class...");
+    }
+
+    public void eatMethod(){
+
+        System.out.println("This is Eat Method of Dog Class .....");
+    }
+
+    public void playMethod(){
+
+        System.out.println("This is Play Method of dog class ....");
+    }
+
+}
