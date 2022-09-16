a8e756aab2e0016c69fada0415dd37ecfe853add
60manojkumar@gmail.com <Motivated@2019>
2022-09-16 08:47:35 +0530

    Calc App

--- src/class_and_objects/Employee.java
+++ src/class_and_objects/Employee.java
@@ -8,6 +8,37 @@ public class Employee {
    String dept="CS";
    double salary=25000.67;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
     public static void main(String[] args) {
 
--- src/methods/CalcApp.java
+++ src/methods/CalcApp.java
@@ -15,31 +15,55 @@ public class CalcApp {
         return  a-b;
     }
 
+    public int multi(int a, int b){
+
+        return  a*b;
+    }
+
+    public double divide(int a, int b){
+
+        return (double)a/b;
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner= new Scanner(System.in);
-
-        System.out.println("Please enter value of a: ");
-        int a= scanner.nextInt();
-
-        System.out.println("Please enter value of b: ");
-        int b= scanner.nextInt();
-
-        System.out.println("Please enter Choice 1: addition , 2 subtraction, 3: for multi and 4 for Divide");
-        int choice=scanner.nextInt();
-
-
         CalcApp obj= new CalcApp();
 
-       if(choice==1) {
-       int result = obj.add(a,b);
-       System.out.println("SUM is "+result);
-       }
-       else if(choice==2){
-           int result = obj.sub(a,b);
-           System.out.println("SUB is "+result);
+        String flag="Y";
 
-       }
+        while (flag=="Y" || flag.equals("Y")){
+            System.out.println("Please enter value of a: ");
+            int a= scanner.nextInt();
+
+            System.out.println("Please enter value of b: ");
+            int b= scanner.nextInt();
+
+            System.out.println("Please enter Choice 1: addition , 2 subtraction, 3: for multi and 4 for Divide");
+            int choice=scanner.nextInt();
+
+            if(choice==1) {
+                int result = obj.add(a,b);
+                System.out.println("SUM is "+result);
+            }
+            else if(choice==2){
+                int result = obj.sub(a,b);
+                System.out.println("SUB is "+result);
+
+            }
+            else if(choice==3){
+                System.out.println("Multi is "+obj.multi(a,b));
+            }
+            else {
+                System.out.println("Division is "+obj.divide(a,b));
+            }
+
+
+            System.out.println("Press Y to continue or press any key to exit from Calculator");
+            Scanner scanner1= new Scanner(System.in);
+            flag=scanner1.nextLine();
+        }
+
 
     }
 }
