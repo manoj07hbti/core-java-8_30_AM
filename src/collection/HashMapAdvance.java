2d9d1272307519cb4c1edac775a6a31e008c54ea
60manojkumar@gmail.com <Motivated@2019>
2022-10-04 08:54:07 +0530

    Advance HashMap

--- src/collection/HashMapAdvance.java
+++ src/collection/HashMapAdvance.java
@@ -4,6 +4,7 @@ import model.Student;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 public class HashMapAdvance {
 
@@ -39,8 +40,39 @@ public class HashMapAdvance {
         studentArrayList.add(student3);
         studentArrayList.add(student4);
 
-        studentMap.put("A",studentArrayList);
+        ArrayList<Student> studentArrayListArt= new ArrayList<>();
 
+        Student student5= new Student("Raj",23,"ART");
+        Student student6= new Student("Rohit", 21,"ART");
+        Student student7= new Student("Jatin",20,"ART");
+        studentArrayListArt.add(student5);
+        studentArrayListArt.add(student6);
+        studentArrayListArt.add(student7);
+
+        studentMap.put("A",studentArrayList);
+        studentMap.put("B",studentArrayListArt);
+
+
+        ArrayList<Student> studentArrayListCS=  studentMap.get("A");
+        for (Student var: studentArrayListCS){
+
+            System.out.println("CS Students :"+var.getName()+" Branch: "+var.getSection());
+        }
+
+
+        //STEP 1 get all the keys of MAP
+
+           Set<String > keys =studentMap.keySet();
+
+          for (String var : keys){
+
+              ArrayList<Student> students= studentMap.get(var);
+
+              for(Student var1: students){
+
+                  System.out.println("Printing entire Map "+var1.getName()+" Section: "+var1.getSection());
+              }
+          }
 
     }
 
